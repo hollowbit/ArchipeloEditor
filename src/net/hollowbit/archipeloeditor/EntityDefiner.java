2cbe4184e539a5ad394f0590dbc67e5be5443868
vedi0boy <vedioboy07@gmail.com>
2017-04-07 13:18:28 -0400

    Added in most property editors. Only need object ones now.

--- src/net/hollowbit/archipeloeditor/EntityDefiner.java
+++ src/net/hollowbit/archipeloeditor/EntityDefiner.java
@@ -1,13 +1,29 @@
 package net.hollowbit.archipeloeditor;
 
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JSpinner;
 import javax.swing.JTextField;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.text.NumberFormatter;
 
 import net.hollowbit.archipeloeditor.entity.EntityType;
+import net.hollowbit.archipeloshared.Direction;
+import net.hollowbit.archipeloshared.PropertyDefinition;
 
 public class EntityDefiner extends JFrame {
-
+	
+	private static final int SPACE_BETWEEN_ELEMENTS = 30;
+	private static final int SPACE_OF_LABELS = 110;
+	private static final int SPACE_FOR_DEFAULT_VALUES = 150;
+	
 	/**
 	 * 
 	 */
@@ -49,7 +65,93 @@ public class EntityDefiner extends JFrame {
 		getContentPane().add(animField);
 		
 		//Properties
+		JLabel defaultsLabel = new JLabel("Properties");
+		defaultsLabel.setBounds(10, SPACE_FOR_DEFAULT_VALUES - 30, 200, 20);
+		getContentPane().add(defaultsLabel);
 		
+		//Add all properties
+		ArrayList<PropertyDefinition> definitions = new ArrayList<PropertyDefinition>();
+		for (PropertyDefinition def : entityType.getData().defaultProperties)
+			definitions.add(def);
+		definitions.addAll(entityType.getData().properties);
+		
+		for (int i = 0; i < definitions.size(); i++) {
+			PropertyDefinition propertyDefinition = definitions.get(i);
+			JLabel label = new JLabel(propertyDefinition.name + (propertyDefinition.required ? "*": "") + ":");
+			label.setBounds(10, i * SPACE_BETWEEN_ELEMENTS + SPACE_FOR_DEFAULT_VALUES, 200, 20);
+			getContentPane().add(label);
+			
+			switch (propertyDefinition.getType()) {
+			case INTEGER:
+				SpinnerModel model = new SpinnerNumberModel(0, Integer.MIN_VALUE, Integer.MAX_VALUE, 1);
+				JSpinner spinner = new JSpinner(model);
+				spinner.setBounds(SPACE_OF_LABELS, i * SPACE_BETWEEN_ELEMENTS + SPACE_FOR_DEFAULT_VALUES, 200, 20);
+				getContentPane().add(spinner);
+				break;
+			case FLOAT:
+				NumberFormat format = NumberFormat.getInstance();
+				NumberFormatter formatter = new NumberFormatter(format);
+				formatter.setValueClass(Float.class);
+				formatter.setMinimum(Float.MIN_VALUE);
+				formatter.setMaximum(Float.MAX_VALUE);
+				formatter.setAllowsInvalid(false);
+				formatter.setCommitsOnValidEdit(true);
+				JFormattedTextField field = new JFormattedTextField(formatter);
+				field.setBounds(SPACE_OF_LABELS, i * SPACE_BETWEEN_ELEMENTS + SPACE_FOR_DEFAULT_VALUES, 200, 20);
+				getContentPane().add(field);
+				break;
+			case DOUBLE:
+				NumberFormat format2 = NumberFormat.getInstance();
+				NumberFormatter formatter2 = new NumberFormatter(format2);
+				formatter2.setValueClass(Double.class);
+				formatter2.setMinimum(Double.MIN_VALUE);
+				formatter2.setMaximum(Double.MAX_VALUE);
+				formatter2.setAllowsInvalid(false);
+				formatter2.setCommitsOnValidEdit(true);
+				JFormattedTextField field2 = new JFormattedTextField(formatter2);
+				field2.setBounds(SPACE_OF_LABELS, i * SPACE_BETWEEN_ELEMENTS + SPACE_FOR_DEFAULT_VALUES, 200, 20);
+				getContentPane().add(field2);
+				break;
+			case POINT:
+				
+				break;
+			case LOCATION:
+				
+				break;
+			case RECTANGLE:
+				
+				break;
+			case DIRECTION:
+				JComboBox<Direction> comboBox = new JComboBox<Direction>();
+				for (Direction direction : Direction.values())
+					comboBox.addItem(direction);
+				comboBox.setSelectedIndex(0);
+				comboBox.setBounds(SPACE_OF_LABELS, i * SPACE_BETWEEN_ELEMENTS + SPACE_FOR_DEFAULT_VALUES, 200, 20);
+				getContentPane().add(comboBox);
+				break;
+			case STRING:
+				JTextField field3 = new JTextField();
+				field3.setBounds(SPACE_OF_LABELS, i * SPACE_BETWEEN_ELEMENTS + SPACE_FOR_DEFAULT_VALUES, 200, 20);
+				getContentPane().add(field3);
+				break;
+			case BOOLEAN:
+				JCheckBox checkBox = new JCheckBox();
+				checkBox.setBounds(SPACE_OF_LABELS, i * SPACE_BETWEEN_ELEMENTS + SPACE_FOR_DEFAULT_VALUES, 200, 20);
+				getContentPane().add(checkBox);
+				break;
+			case JSON:
+				
+				break;
+			case STYLE:
+				JComboBox<Integer> comboBox2 = new JComboBox<Integer>();
+				for (int u = 0; u < entityType.getData().numberOfStyles; u++)
+					comboBox2.addItem(u);
+				comboBox2.setSelectedIndex(0);
+				comboBox2.setBounds(SPACE_OF_LABELS, i * SPACE_BETWEEN_ELEMENTS + SPACE_FOR_DEFAULT_VALUES, 200, 20);
+				getContentPane().add(comboBox2);
+				break;
+			}
+		}
 		
 	}
 
