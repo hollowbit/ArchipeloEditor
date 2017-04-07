13757c75d5ebd798c4278fc5cfec082c8288a8b8
vedi0boy <vedioboy07@gmail.com>
2017-04-07 10:12:43 -0400

    Worked on entity definer.

--- src/net/hollowbit/archipeloeditor/EntityDefiner.java
+++ src/net/hollowbit/archipeloeditor/EntityDefiner.java
@@ -1,6 +1,8 @@
 package net.hollowbit.archipeloeditor;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import net.hollowbit.archipeloeditor.entity.EntityType;
 
@@ -10,29 +12,45 @@ public class EntityDefiner extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	private float x, y;
-	private EntityType entityType;
-	private MainEditor editor;
-	
 	public EntityDefiner (MainEditor editor, EntityType entityType, float x, float y) {
 		setAlwaysOnTop(true);
 		setResizable(false);
 		setTitle("Entity Definer");
-		setBounds(100, 100, 500, 700);
+		setBounds(100, 100, 325, 500);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setIconImage(MainEditor.ICON);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(null);
 		
-		this.editor = editor;
-		this.entityType = entityType;
-		this.x = x;
-		this.y = y;
-		
 		//TODO Add base entity settings
 		//TODO Add default properties
 		//TODO Add other properties
+		
+		JLabel requiredLabel = new JLabel("* Required");
+		requiredLabel.setBounds(10, 10, 200, 30);
+		getContentPane().add(requiredLabel);
+		
+		//ID
+		JLabel idLabel = new JLabel("Unique ID*: ");
+		idLabel.setBounds(10, 30, 200, 30);
+		getContentPane().add(idLabel);
+		
+		JTextField idField = new JTextField();
+		idField.setBounds(100, 35, 200, 20);
+		getContentPane().add(idField);
+		
+		//Animation
+		JLabel animLabel = new JLabel("Animation: ");
+		animLabel.setBounds(10, 60, 200, 30);
+		getContentPane().add(animLabel);
+		
+		JTextField animField = new JTextField();
+		animField.setBounds(100, 65, 200, 20);
+		getContentPane().add(animField);
+		
+		//Properties
+		
+		
 	}
 
 }
